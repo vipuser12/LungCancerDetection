947926875759ccddaa10a44f3c51a0b84d50b7e5
vipuser12 <149406262+vipuser12@users.noreply.github.com>
2024-12-01 10:01:48 +0200

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/LungCancerDetection/',  // Make sure the repository name matches here
-  build: {
-    outDir: 'dist',
-  },
+  base: '/LungCancerDetection/', // Set the base path for GitHub Pages
 });
